ec5acbfa9e4da728678048b0dd5692a3899f2023
Jakob Winkler <jakob545@gmail.com>
2024-11-22 21:45:26 +0100

    Add working counter react

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className='App'>
+      <div className='logo-box'>
+        <a href='https://github.com/electron-vite/electron-vite-react' target='_blank'>
+        </a>
+      </div>
+      <h1>Electron + Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Electron + Vite logo to learn more
+      </p>
+      <div className='flex-center'>
+        Place static files into the<code>/public</code> folder
+      </div>
+    </div>
+  )
+}
+
+export default App
